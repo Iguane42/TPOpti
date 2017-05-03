dcefd3ac7fc0b1f60c35d849f93be6e9bc4d8c8f
Epulapp <Epulapp@EPULAPP71.univ-lyon1.fr>
2017-05-03 11:17:58 +0200

    Ajout constructeur

--- src/Jeu/Plateau.java
+++ src/Jeu/Plateau.java
@@ -11,4 +11,20 @@ package Jeu;
  */
 public class Plateau {
     
+    private int nbDame;
+    private int taille;
+    private int[] posDame;
+    
+    public Plateau(int nbDame){
+        this.nbDame = nbDame;
+        this.taille = nbDame; // Car la taille du plateau est égale à nXn
+        this.posDame = new int[this.nbDame]; // Initialise le tableau des positons
+        
+        // Initialise le tableau des positions des dames en diagonale (haut gauche vers bas droit)
+        for(int i = 0; i < this.nbDame; i++){
+            this.posDame[i] = i;
+        }
+    }
+    
+    
 }
